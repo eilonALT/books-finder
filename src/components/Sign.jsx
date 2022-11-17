3dbc279559c56297536b7cc7f41ca591bd685e93
eilon <eilon159123@gmail.com>
2022-11-17 18:57:49 +0200

    css

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -12,23 +12,23 @@ const Sign = () => {
     }
     return (
         <div className="sign">
-            <h1>Books Finder</h1>
+            <h1 >Books Finder</h1>
             <h2>Find your favorite books</h2>
-            <div className="form">
-                <h3>Sign In</h3>
+            <div className="sign-form">
+                <h1>Sign In now !</h1>
                 <input type="text" placeholder="user name" onChange={(e) => {
                     setUsername(e.target.value)
                     // console.log(username)
                 }} />
-                <br/>
+                <br />
                 <input type="password" placeholder="password" onChange={(e) => {
                     setPassword(e.target.value)
                     console.log(password)
                 }} />
-                <br/>
-                <button onClick={Signin}>Sign In</button>
-                <br/>
-                <button>Sign Up</button>
+                <div className="form-buttons">
+                    <button onClick={Signin}>Sign In</button>
+                    <button>Sign Up</button>
+                </div>
             </div>
         </div>
     )
