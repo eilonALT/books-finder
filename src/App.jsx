0fac134b5dfebfc7be2a3cd961b012ab858e20b2
eilon <eilon159123@gmail.com>
2022-11-18 08:01:31 +0200

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Form
 } from 'react-router-dom';
 
 import './App.css';
@@ -11,18 +12,22 @@ import Home from './components/Home';
 import Search from './components/Search';
 import WishList from './components/WishList';
 import Sign from './components/Sign';
-function App() {
+import Error from './components/Error';
+
+const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-        <Route path="/" element={<Sign/>}>
+          <Route path="/" element={<Sign />}>
           </Route>
-          <Route path="/search" element={<Search/>}>
+          <Route path="/search" element={<Search />}>
           </Route>
-          <Route path="/wishlist" element={<WishList/>}>
+          <Route path="/wishlist" element={<WishList />}>
           </Route>
-          <Route path="/home" element={<Home/>}>
+          <Route path="/home" element={<Home />}>
+          </Route>
+          <Route path="/*" element={<Error />}>
           </Route>
         </Routes>
       </div>
